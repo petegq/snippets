1a61caa98ae81b9dced948a34769a8bd88e09c13
Peter Garside-Quin <petegq@gmail.com>
2023-03-21 13:21:34 +0000

    Tidy

--- xhr.js
+++ xhr.js
@@ -1,11 +1,11 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function reqListener() {
+function listener() {
   console.log(this.responseText);
 }
 
 const xhr = new XMLHttpRequest();
-xhr.addEventListener("load", reqListener);
+xhr.addEventListener("load", listener);
 xhr.open("GET", "http://www.example.org/example.txt");
 xhr.send();
 
