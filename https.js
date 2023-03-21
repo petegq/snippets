f044417ef0c61ebaedbc385612606342921a3bea
Peter Garside-Quin <petegq@gmail.com>
2023-03-21 13:56:07 +0000

    Https example

---
+++ https.js
@@ -0,0 +1,26 @@
+import https from "https";
+
+const options = {
+  hostname: "example.org",
+  method: "GET",
+};
+
+const req = https.request(options, (res) => {
+  let data = "";
+
+  res.on("data", (chunk) => {
+    data += chunk;
+  });
+
+  res.on("end", () => {
+    console.log(data);
+    // If you want to parse the data as json uncomment the following
+    // console.log(JSON.parse(data));
+  });
+});
+
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.end();
