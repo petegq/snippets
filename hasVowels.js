3a103c237a5a3afc44708ed68601413fb4b9ac3a
Peter Garside-Quin <petegq@gmail.com>
2024-06-06 22:01:29 +0100

    Does the sting have vowels concisely

--- hasVowels.js
+++ hasVowels.js
@@ -1,18 +1,10 @@
 const word = "String"
 const map = Array.prototype.map
 
-// const stringArray = map.call(word, eachLetter => {
-//     return `${eachLetter}`
-// })
-
-const isVowel = map.call(word, eachLetter => {
+const hasVowels = map.call(word, eachLetter => {
     return /[aeiou]/.test(eachLetter)
-})
+}).some(vowel => vowel === true)
 
 console.log(isVowel)
-// Output: [ false, false, false, true, false, false ]
-
-const hasVowels = isVowel.some(vowel => vowel === true)
-console.log(hasVowels)
 // Output: true
 
